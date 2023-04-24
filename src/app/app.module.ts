7d8ec14e112916f05588d0aeeb161503a4177e1b
osmanjosue <osmanjosue@gmail.com>
2023-04-24 00:41:06 -0600

    55 seccion, 195 clase, se llamo y uso la api resreq.in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { routing, appRoutingProviders } from './app.routing';
+import { HttpClientModule} from '@angular/common/http'; 
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,6 +11,8 @@ import { VideojuegoComponent } from './videojuego/videojuego.component';
 import { ZapatillasComponent } from './zapatillas/zapatillas.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { HomeComponent } from './home/home.component';
+import { ExternoComponent } from './externo/externo.component';
+import { PeticionesService } from './services/peticiones.service';
 
 @NgModule({
   declarations: [
@@ -17,15 +20,17 @@ import { HomeComponent } from './home/home.component';
     VideojuegoComponent,
     ZapatillasComponent,
     CursosComponent,
-    HomeComponent
+    HomeComponent,
+    ExternoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    routing
+    routing, 
+    HttpClientModule
   ],
-  providers: [appRoutingProviders],
+  providers: [appRoutingProviders,PeticionesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
